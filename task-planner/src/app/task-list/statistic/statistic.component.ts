077a4b3a61695f15c01739415d0842b82d872fca
gmsmirnov <artress@ngs.ru>
2019-08-04 02:30:07 +0700

    Связывание двух компонентов. [#62341]

--- task-planner/src/app/task-list/statistic/statistic.component.ts
+++ task-planner/src/app/task-list/statistic/statistic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-statistic',
@@ -7,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticComponent implements OnInit {
 
-  tasksAll = 15;
-  tasksPlanned = 5;
-  tasksOverdue = 6;
-  tasksFinished = 5;
+  @Input() tasksAll;
+  @Input() tasksPlanned;
+  @Input() tasksOverdue;
+  @Input() tasksFinished;
 
   constructor() { }
 
--- task-planner/src/app/task-list/task-list.component.ts
+++ task-planner/src/app/task-list/task-list.component.ts
@@ -6,6 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./task-list.component.css']
 })
 export class TaskListComponent implements OnInit {
+  tasks = [
+    {
+      name: 'Пройти раздел \"Базовый синтаксис\"',
+      category: 'Angular',
+      dateStart: '14/07/2019',
+      dateEnd: '25/07/2019',
+      status: 'Выполнено'
+    },
+    {
+      name: 'Пройти раздел \"Дериктивы и пайпы\"',
+      category: 'Angular',
+      dateStart: '26/07/2019',
+      dateEnd: '02/08/2019',
+      status: 'Просрочено'
+    },
+    {
+      name: 'Пройти раздел \"Внедрение зависимостей\"',
+      category: 'Angular',
+      dateStart: '03/08/2019',
+      dateEnd: '07/08/2019',
+      status: 'Запланировано'
+    },
+    {
+      name: 'Пройти раздел \"Маршрутизация, модули\"',
+      category: 'Angular',
+      dateStart: '08/08/2019',
+      dateEnd: '10/08/2019',
+      status: 'Запланировано'
+    },
+    {
+      name: 'Пройти раздел \"Работа с формами\"',
+      category: 'Angular',
+      dateStart: '10/08/2019',
+      dateEnd: '19/08/2019',
+      status: 'Запланировано'
+    }
+  ];
 
   constructor() { }
 
@@ -18,5 +55,17 @@ export class TaskListComponent implements OnInit {
 
   filterTasks($event) {
     console.log($event.target.checked);
+  }
+
+  getTasksListSize() {
+    return this.tasks.length;
+  }
+
+  getTasksAmountByStatus(status: string) {
+    return (this.tasks.filter(task => task.status === status)).length;
+  }
+
+  deleteTaskFromArray(name: string) {
+    console.log('Задача ' + name + ' удалена');
   }
 }
